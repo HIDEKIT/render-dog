42553752503c20dc383887efea58f683d28d8f1b
HIDEKIT <hidekitakeda101@gmail.com>
2025-06-23 17:57:07 +0900

    “second“

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel　でーす
                 </div>
 
                 <div class="links">
