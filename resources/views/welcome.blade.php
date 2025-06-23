b12d014e57cea8c30b92401590ea9367e597a163
HIDEKIT <hidekitakeda101@gmail.com>
2025-06-23 18:03:50 +0900

    “2025/6/232”

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel　でーす
+                    Laravel　でーすよ
                 </div>
 
                 <div class="links">
